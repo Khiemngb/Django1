15824c6c579631bbe3847f19146ef1cde4dcbbed
Khiem Nguyen <ngbinhkhiem1611@gmail.com>
2021-09-19 16:14:58 +0700

    create

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+# Function will return to client
+def home_page(request):
+    return HttpResponse('Hello World!')
+
+def about_page(request):
+    return HttpResponse('about home')
+
